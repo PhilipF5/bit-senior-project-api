85a8dfe3b41a14609ea9df5ad1196c64f3b0157e
Philip Fulgham <philfulgham@gmail.com>
2017-03-18 19:47:55 -0400

    Added Login methods, closes #2

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -8,59 +8,21 @@ using System.Data;
 
 namespace api.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("api/login")]
 	public class LoginController : Controller
 	{
 		[HttpPost("mobile")]
-		public string Mobile([FromBody] string[] creds)
+		public Login Mobile([FromBody] string creds)
 		{
-			using (var connection = new SqlConnection("Server=tcp:auctionit.database.windows.net,1433;Initial Catalog=auctionitdb;Persist Security Info=False;User ID=bit4454;Password=4Fy>oj@8&8;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
-			{
-				var command = new SqlCommand("SELECT * FROM Users WHERE Username = @user", connection);
-				command.Parameters.Add("@user", SqlDbType.VarChar).Value = creds[0];
-				connection.Open();
-				using (var reader = command.ExecuteReader())
-				{
-					while (reader.Read())
-					{
-						if (reader[2].ToString() == creds[1])
-						{
-							return reader[4].ToString();
-						}
-						else
-						{
-							return "Invalid password";
-						}
-					}
-				}
-			}
-			return "Invalid username";
+			string[] credsArray = creds.Split(' ');
+			return new Login(credsArray[0], credsArray[1]);
 		}
 
 		[HttpPost("web")]
-		public string Web([FromBody] string[] creds)
+		public Login Web([FromBody] string creds)
 		{
-			using (var connection = new SqlConnection("Server=tcp:auctionit.database.windows.net,1433;Initial Catalog=auctionitdb;Persist Security Info=False;User ID=bit4454;Password=4Fy>oj@8&8;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
-			{
-				var command = new SqlCommand("SELECT * FROM Admins WHERE Username = @user", connection);
-				command.Parameters.Add("@user", SqlDbType.VarChar).Value = creds[0];
-				connection.Open();
-				using (var reader = command.ExecuteReader())
-				{
-					while (reader.Read())
-					{
-						if (reader[2].ToString() == creds[1])
-						{
-							return reader[4].ToString();
-						}
-						else
-						{
-							return "Invalid password";
-						}
-					}
-				}
-			}
-			return "Invalid username";
+			string[] credsArray = creds.Split(' ');
+			return new Login(credsArray[0], credsArray[1], "admin");
 		}
 	}
 }
